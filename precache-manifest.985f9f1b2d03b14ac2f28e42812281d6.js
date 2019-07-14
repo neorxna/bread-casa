a730439e3ea4f113293ac586cf8dee798471dee4
arron <a.mclaughlin@auckland.ac.nz>
2019-07-14 19:55:00 +1200

    build

---
+++ precache-manifest.985f9f1b2d03b14ac2f28e42812281d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96cfa517a482ef0aa7a1dd636cc1f20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13508b19f46d7c9238b8",
+    "url": "/static/css/main.2c178911.chunk.css"
+  },
+  {
+    "revision": "45b3287fd965aafe0a05",
+    "url": "/static/js/2.911f690b.chunk.js"
+  },
+  {
+    "revision": "13508b19f46d7c9238b8",
+    "url": "/static/js/main.2942b60c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
